5882676a34103d07647c089466c4de1da46f0f4e
asus <mehdimohajeri78@gmail.com>
2022-12-05 22:24:19 +0330

    Add pre processing method

--- BubbleSheetGrader.py
+++ BubbleSheetGrader.py
@@ -1,11 +1,23 @@
-import numpy as np
 import cv2
 
 class BubbleSheetGrader:
+    """Class for reading image with BubbleSheet answer and grading that."""
     def __init__(self, path):
-        pass
+        """Load and resize image"""
+        self.image = cv2.imread(path)
+        self.image = cv2.resize(self.image, (780, 540))
+        self.pre_processed_image = None
+
     def pre_process(self):
-        pass
+        """Pre processing image"""
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        de_noised = cv2.fastNlMeansDenoising(gray, None, 2, 5, 10)
+        blurred = cv2.GaussianBlur(de_noised,(3, 3), 0)
+        self.pre_processed_image = blurred
+
     def detect_answer_sheet(self):
-        pass
-    
+        """Detect answer sheet in image"""
+
+
+bsg = BubbleSheetGrader('data/camScanedSample/sample (4).png')
+bsg.pre_process()
