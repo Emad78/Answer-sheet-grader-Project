1d0567c291e3bea79b9e9fc126d61de874ea9cb2
asus <mehdimohajeri78@gmail.com>
2022-12-05 11:56:23 +0330

    Setub and Import libraries

---
+++ BubbleSheetGrader.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+class BubbleSheetGrader:
+    def __init__(self, path):
+        pass
+    def pre_process(self):
+        pass
+    def detect_answer_sheet(self):
+        pass
+    
