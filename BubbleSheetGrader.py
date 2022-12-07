3b1885b37d0ec5aed16784c0f70ee07511e6e6f4
asus <mehdimohajeri78@gmail.com>
2022-12-07 17:11:58 +0330

    Detecting answer box for specific answer-sheet

--- BubbleSheetGrader.py
+++ BubbleSheetGrader.py
@@ -1,4 +1,6 @@
 import cv2
+import pickle
+import numpy as np
 
 class BubbleSheetGrader:
     """Class for reading image with BubbleSheet answer and grading that."""
@@ -6,6 +8,15 @@ class BubbleSheetGrader:
         """Load and resize image"""
         self.image = cv2.imread(path)
         self.image = cv2.resize(self.image, (780, 540))
+        self.box_template = []
+        with open('data/template/rectangle.temp', 'rb') as f:
+           self.box_template.append(pickle.load(f))
+        with open('data/template/square.temp', 'rb') as f:
+           self.box_template.append(pickle.load(f))
+        with open('data/template/bubble.temp', 'rb') as f:
+           self.bubble = pickle.load(f)
+           
+           
         self.pre_processed_image = None
 
     def pre_process(self):
@@ -22,20 +33,22 @@ class BubbleSheetGrader:
         """Detect answer box in answer sheet"""
         edged = cv2.Canny(self.pre_processed_image, 10, 100)
         contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        contours = sorted(contours, key=cv2.contourArea, reverse=True)
-        cv2.drawContours(self.image, contours, -1, (0, 0, 0), 2)
-        self.show_image(self.image)
-        for i in range(len(contours)):
-            cp = self.image.copy()
-            print(cv2.contourArea(contours[i]))
-            cv2.drawContours(cp, contours[:i+1], -1, (255, 0, 0), 2)
-            self.show_image(cp)
-            
+
+        box_contours = list(filter(lambda x: cv2.contourArea(x)>500, contours))
+        box_contours = sorted(box_contours, key=lambda x: cv2.contourArea(x), reverse=True)
+        answer_box = self.crop_contour_area(box_contours[0], self.image)
+        self.show_image(answer_box)
+        return answer_box
+    
+    def crop_contour_area(self, contour, image):
+        cropped_contours = []
+        x,y,w,h= cv2.boundingRect(contour)
+        return image[y:y+h, x:x+w]
     def show_image(self, img):
         cv2.imshow("show", img)
         cv2.waitKey(0)
         
 
-bsg = BubbleSheetGrader('data/standardSample/sample (2).bmp')
+bsg = BubbleSheetGrader('data/standardSample/sample (7).bmp')
 bsg.pre_process()
 bsg.detect_answer_box()
