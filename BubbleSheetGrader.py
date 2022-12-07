3c32323bfc1de4d0cbe7d3f9f06ce2599cd85cdb
asus <mehdimohajeri78@gmail.com>
2022-12-07 20:21:30 +0330

    Improve answer box detection accuracy

--- BubbleSheetGrader.py
+++ BubbleSheetGrader.py
@@ -1,13 +1,14 @@
 import cv2
 import pickle
 import numpy as np
+import imutils
 
 class BubbleSheetGrader:
     """Class for reading image with BubbleSheet answer and grading that."""
     def __init__(self, path):
         """Load and resize image"""
         self.image = cv2.imread(path)
-        self.image = cv2.resize(self.image, (780, 540))
+        self.image = imutils.resize(self.image, height = 1000)
         self.box_template = []
         with open('data/template/rectangle.temp', 'rb') as f:
            self.box_template.append(pickle.load(f))
@@ -22,8 +23,7 @@ class BubbleSheetGrader:
     def pre_process(self):
         """Pre processing image"""
         gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-        de_noised = cv2.fastNlMeansDenoising(gray, None, 2, 5, 10)
-        blurred = cv2.GaussianBlur(de_noised,(3, 3), 0)
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
         self.pre_processed_image = blurred
 
     def detect_answer_sheet(self):
@@ -31,24 +31,32 @@ class BubbleSheetGrader:
     
     def detect_answer_box(self):
         """Detect answer box in answer sheet"""
-        edged = cv2.Canny(self.pre_processed_image, 10, 100)
-        contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        edged = cv2.Canny(self.pre_processed_image, 75, 200)
+        contours = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+        contours = sorted(contours, key = cv2.contourArea, reverse = True)
+        screenCnt = []
 
-        box_contours = list(filter(lambda x: cv2.contourArea(x)>500, contours))
-        box_contours = sorted(box_contours, key=lambda x: cv2.contourArea(x), reverse=True)
-        answer_box = self.crop_contour_area(box_contours[0], self.image)
-        self.show_image(answer_box)
+        for c in contours:
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.002 * peri, True)
+            if len(approx) == 4:
+                screenCnt.append(approx)
+        
+        answer_box = self.crop_contour_area(screenCnt[0], self.pre_processed_image)
         return answer_box
     
     def crop_contour_area(self, contour, image):
-        cropped_contours = []
+        """Crop contour area from input image"""
         x,y,w,h= cv2.boundingRect(contour)
         return image[y:y+h, x:x+w]
+    
     def show_image(self, img):
+        """Using for show results"""
         cv2.imshow("show", img)
         cv2.waitKey(0)
         
 
 bsg = BubbleSheetGrader('data/standardSample/sample (7).bmp')
 bsg.pre_process()
-bsg.detect_answer_box()
+bsg.show_image(bsg.detect_answer_box())
